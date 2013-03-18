cb6178c141453d8ca8b1c9335d4b8b75b66e0167
Teijo Laine <teijo@aropupu.fi>
2013-03-18 23:23:27 +0200

    Extact plugin code to a separate JS file

---
+++ jquery.floodtext.js
@@ -0,0 +1,26 @@
+;(function($) {
+  $.fn.textFlood = function(options) {
+    var THRESHOLD = 4
+    return this.each(function() {
+      var $container = $(this)
+      var height = $container.height()
+      var width = $container.width()
+      var content = $container.contents()
+      var $wrap = $('<span>').append(content)
+      $container.append($wrap)
+      ;(function f(size, scan) {
+        $wrap.css('font-size', size)
+        if ($wrap.height() > height || $wrap.width() > width)
+          size -= scan
+        else
+          size += scan
+        if (size > 5 && Math.abs(scan) > THRESHOLD)
+          f(size, ~~(scan/2))
+        else {
+          $wrap.replaceWith(content)
+          $container.css('font-size', size)
+        }
+      })(height, height/2)
+    })
+  }
+})(jQuery);
